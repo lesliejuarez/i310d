6d526e3d93d280d4d38c4224abbc6dca8b903a89
leslie <lesliejuarez@utexas.edu>
2024-11-14 21:45:38 -0600

    added volume of sphere

---
+++ volume_of_sphere.py
@@ -0,0 +1,12 @@
+import math
+
+def calculate_volume_sphere(radius):
+	return (4/3) * math.pi * radius ** 3
+
+result = calculate_volume_of_sphere(30)
+result = calculate_volume_of_sphere(40)
+
+print(result)
+
+
+	
